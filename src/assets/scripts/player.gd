62475f9fc5babbcbe0d5c1b7f54cfc83e9dc3972
Quinten Marsala <quinten.marsala@gmail.com>
2024-11-01 16:36:33 -0700

    diagnol movement

--- src/assets/scripts/player.gd
+++ src/assets/scripts/player.gd
@@ -1,24 +1,32 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
+const SPEED = 100.0
 
 @onready var character_sprite: AnimatedSprite2D = $CharacterSprite
 
 func _physics_process(delta: float) -> void:
-	var xDirection := Input.get_axis("move_west", "move_east")
-	if xDirection:
-		velocity.x = xDirection * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	var yDirection := Input.get_axis("move_north", "move_south")
-	if yDirection:
-		velocity.y = yDirection * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-	
+	var xDirection = get_x_input()
+	var yDirection = get_y_input()
+	velocity = calc_player_velocity(xDirection, yDirection)
 	play_animation(xDirection, yDirection)
 	move_and_slide()
+
+func get_x_input():
+	var xDirection := Input.get_axis("move_west", "move_east")
+	if xDirection:
+		return xDirection
+	else:
+		return move_toward(velocity.x, 0, SPEED)
+
+func get_y_input():
+	var yDirection = Input.get_axis("move_north", "move_south")
+	if yDirection:
+		return yDirection
+	else:
+		return move_toward(velocity.y, 0, SPEED)
+
+func calc_player_velocity(xDirection, yDirection):
+	return Vector2(xDirection, yDirection).normalized() * SPEED
 
 func play_animation(xDirection, yDirection): 
 	if xDirection == 0 and yDirection == 0:
