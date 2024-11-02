549cd470457ae3493a50d5506392496746b2ccf3
Quinten Marsala <quinten.marsala@gmail.com>
2024-11-01 23:06:55 -0700

    add some attack code

--- src/assets/scripts/player.gd
+++ src/assets/scripts/player.gd
@@ -1,14 +1,17 @@
 extends CharacterBody2D
 
 const SPEED = 100.0
+const CHARGED_ATTACK_TIME = .5
+const BASIC_COOLDOWN = .3
+const CHARGED_COOLDOWN = 0
+const HEAVY_COOLDOWN = .7
 
 @onready var character_sprite: AnimatedSprite2D = $CharacterSprite
+@onready var cooldown_timer: Timer = $CooldownTimer
+
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("basic_attack"):
-		print("basic attack")
-	if Input.is_action_just_pressed("heavy_attack"):
-		print("heavy attack")
+	handle_attack_action(delta)
 
 func _physics_process(delta: float) -> void:
 	var xDirection = get_x_input()
@@ -40,3 +43,34 @@ func play_animation(xDirection, yDirection):
 	else:
 		character_sprite.flip_h = xDirection < 0
 		character_sprite.play("run")
+
+var basic_pressed_time = 0
+var attack_is_cooling_down = false
+func handle_attack_action(delta):
+	if attack_is_cooling_down:
+		return
+	if Input.is_action_pressed("basic_attack"):
+		basic_pressed_time += delta
+		
+	var basic_attack_released = Input.is_action_just_released("basic_attack")
+	var heavy_attack_released = Input.is_action_just_released("heavy_attack")
+	var attack_released = basic_attack_released or heavy_attack_released
+	if attack_released:
+		attack_is_cooling_down = true
+		attack_target(basic_attack_released, heavy_attack_released, "todo")
+
+func attack_target(basic_attack_released, heavy_attack_released, target):
+	if basic_attack_released:
+		if basic_pressed_time > CHARGED_ATTACK_TIME: 
+			cooldown_timer.start(CHARGED_COOLDOWN)
+			print("charged_attack")
+		else:
+			cooldown_timer.start(BASIC_COOLDOWN)
+			print("basic_attack")
+		basic_pressed_time = 0
+	if heavy_attack_released:
+		cooldown_timer.start(HEAVY_COOLDOWN)
+		print("heavy_attack")
+
+func _on_cooldown_timer_timeout() -> void:
+	attack_is_cooling_down = false
