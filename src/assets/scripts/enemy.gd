ef997ad478e078ef1c69763d6b568975ea0522ba
Quinten Marsala <quinten.marsala@gmail.com>
2024-11-03 15:56:06 -0800

    add basic enemy agro

--- src/assets/scripts/enemy.gd
+++ src/assets/scripts/enemy.gd
@@ -1,13 +1,16 @@
-extends Node
+extends CharacterBody2D
 class_name Enemy
 
 const STARTING_HP = 5
+const SPEED = 100.0
 
 @onready var animations: AnimatedSprite2D = $Animations
 @onready var timer: Timer = $Timer
 
 var health = STARTING_HP
 var is_dead = false
+var player_is_visible = false
+var player
 
 func _process(delta: float) -> void:
 	if is_dead: return
@@ -20,6 +23,15 @@ func _process(delta: float) -> void:
 	if not animations.is_playing():
 		animations.play("idle")
 
+
+func _physics_process(delta: float) -> void:
+	if is_dead or not player_is_visible:
+		return 
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction.normalized() * SPEED
+	move_and_slide()
+
+
 func receive_damage(damage):
 	if is_dead: return 
 
@@ -29,3 +41,14 @@ func receive_damage(damage):
 
 func _on_timer_timeout() -> void:
 	queue_free()
+
+
+func _on_vison_area_body_entered(body: Node2D) -> void:
+	player_is_visible = true
+	player = body
+	print("see player")
+
+
+func _on_vison_area_body_exited(body: Node2D) -> void:
+	player_is_visible = false
+	print("player is gone")
