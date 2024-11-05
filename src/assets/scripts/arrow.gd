1bad2e6f1e9720092a2ae22caae16639f9623d64
Quinten Marsala <quinten.marsala@gmail.com>
2024-11-04 21:59:24 -0800

    give arrows lifetime

--- src/assets/scripts/arrow.gd
+++ src/assets/scripts/arrow.gd
@@ -13,9 +13,12 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		body.receive_damage(2)
 	queue_free()
 	
-func shoot_arrow(starting_pos, target):
+func init(starting_position, target):
 	# todo: is it possible to add it to the scene tree from here?
-	position = starting_pos
+	position = starting_position
 	look_at(target)
 	rotate(deg_to_rad(90))
 	direction = global_position.direction_to(target)
+
+func _on_timer_timeout() -> void:
+	queue_free()
