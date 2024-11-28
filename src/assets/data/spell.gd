c4c7fcbf3f34609edc820e3de3cb9f677c1be943
Quinten Marsala <quinten.marsala@gmail.com>
2024-11-27 19:45:59 -0800

    use a resource for spell data

---
+++ src/assets/data/spell.gd
@@ -0,0 +1,9 @@
+class_name SpellData
+extends Resource
+
+@export var spell: PackedScene
+@export var cast_time = .25
+@export var cooldown = 1.0
+@export var attack_action = "heavy_attack"
+
+#todo: model spell damage, need DoT vs Regular SpellDatas
