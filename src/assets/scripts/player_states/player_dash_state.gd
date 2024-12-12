bc9f55b7efd5f2ed67f2775295f9669cb1adec05
Quinten Marsala <quinten.marsala@gmail.com>
2024-12-11 16:49:39 -0800

    dash through, and secondary player layer for non enemies to look for

--- src/assets/scripts/player_states/player_dash_state.gd
+++ src/assets/scripts/player_states/player_dash_state.gd
@@ -3,18 +3,24 @@ class_name PlayerDashState
 
 func enter():
 	print("dash")
+	player.set_collision_layer_value(2, false)
+	player.set_collision_mask_value(3, false)
 	player.is_dash_cooldown = true
 	player.velocity = player.global_position.direction_to(player.move_target).normalized() * (player.SPEED * player.DASH_MULTIPLIER)
 	player.dash_timer.start(player.DASH_TIME)
 	player.character_sprite.play("dash")
+	
 
 func exit():
+	player.set_collision_layer_value(2, true)
+	player.set_collision_mask_value(3, true)
 	player.move_target = player.global_position
 	player.velocity = Vector2.ZERO
 	player.move_destination_indicator.hide()
 	player.dash_timer.stop()
 	player.dash_cooldown_timer.start(player.DASH_COOLDOWN)
 	player.character_sprite.stop()
+
 
 func handle_movement_input(delta):
 	pass
