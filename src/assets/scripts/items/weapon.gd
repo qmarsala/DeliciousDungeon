c2743ac11794ecf95c8c809cf73c6111653976f9
Quinten Marsala <quinten.marsala@gmail.com>
2024-12-27 15:46:23 -0800

    aiming for bow - NEEDS REFAC

--- src/assets/scripts/items/weapon.gd
+++ src/assets/scripts/items/weapon.gd
@@ -2,10 +2,13 @@ class_name Weapon
 extends Node2D
 
 var player: Player
+# weapon data?
 @export var max_range: float
+@export var aim_follows_cursor: bool
 
 # todo: animations - see move state in player
 @onready var sprite = $Sprite2D
+@onready var animations: AnimatedSprite2D = $Animations
 
 func init(p: Player):
 	player = p
@@ -19,7 +22,20 @@ func _process(delta: float) -> void:
 		$AttackIndicator.hide()
 	elif !$AttackIndicator.visible:
 		$AttackIndicator.show()
-	$AttackIndicator.global_position = get_attack_location()
+	var location = get_attack_location()
+	$AttackIndicator.global_position = location
+	if aim_follows_cursor:
+		$AttackIndicator.rotation_degrees = 90
+		#this type of state makes me want scripts for the weapons indicators that can vary
+		# maybe strategy pattern?
+		# I think this worked well on the skeleton because of the 'aim vector'
+		look_at(location)
+		# trying this in player instead
+		var direction = location - player.hand.global_position
+		var target_location = location
+		if direction.length() > 4:
+			target_location = player.hand.global_position + (direction.normalized() * 4)
+			global_position = target_location
 
 func get_attack_location() -> Vector2:
 	var mouse_pos = player.get_global_mouse_position()
