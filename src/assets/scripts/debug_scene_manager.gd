2dbed2c2aa4ae6b8d8d499a29cc3debc3f0f94ed
Quinten Marsala <quinten.marsala@gmail.com>
2024-12-27 10:43:13 -0800

    show damage number in debug scene

--- src/assets/scripts/debug_scene_manager.gd
+++ src/assets/scripts/debug_scene_manager.gd
@@ -2,11 +2,21 @@ extends Node2D
 
 @export var player_scene: PackedScene
 
+@export var damage_number: PackedScene
+
 var player: Player
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("Player")
 	player.PlayerDied.connect(_on_player_died)
+	SignalBusService.DamageReceived.connect(_add_damage_number)
 
 func _on_player_died() -> void:
 	get_tree().reload_current_scene()
+
+# this should be a component?
+func _add_damage_number(damage: float, position: Vector2):
+	var instance = damage_number.instantiate() as Label
+	instance.text = String.num(damage)
+	instance.global_position = position
+	add_child(instance)
