d4bbb99cba8f984d7e01b0ac457dade7afc5fd8b
Quinten Marsala <quinten.marsala@gmail.com>
2024-12-27 10:43:02 -0800

    add an attack dummy script for testing new attacks

---
+++ src/assets/scripts/attack_dummy.gd
@@ -0,0 +1,10 @@
+extends Sprite2D
+
+@onready var status_effects_component: StatusEffectComponent = $StatusEffectsComponent
+
+func _ready() -> void:
+	add_to_group(Interfaces.Damageable)
+	status_effects_component.init(self)
+	
+func receive_damage(damage):
+	SignalBusService.DamageReceived.emit(damage, global_position)
