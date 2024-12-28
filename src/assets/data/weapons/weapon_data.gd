2b3aff809b8bb3292cfae4873a885158d92ddc7a
Quinten Marsala <quinten.marsala@gmail.com>
2024-12-28 13:18:54 -0800

    create some level one weapons

---
+++ src/assets/data/weapons/weapon_data.gd
@@ -0,0 +1,7 @@
+class_name WeaponData
+extends Resource
+
+@export var max_range: float
+#what other ways can we have weapon stats. 
+# attack speed? which has an affect on the cast times and/or cooldowns?
+@export var weapon_abilities: Array[WeaponAbilityData]
