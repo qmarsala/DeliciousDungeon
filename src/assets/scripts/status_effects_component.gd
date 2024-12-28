fa54c7d53cb99e018edd3c2d1d60f8be1b57fc53
Quinten Marsala <quinten.marsala@gmail.com>
2024-12-27 16:40:40 -0800

    fix statuses

--- src/assets/scripts/status_effects_component.gd
+++ src/assets/scripts/status_effects_component.gd
@@ -21,13 +21,17 @@ func _process(delta: float) -> void:
 
 # pulling these into a component isnt helping - since a parent script
 # may still need to accept calls for these methods?
-func has_effect(effect: StatusEffect):
-	return status_effects.has(effect)
+func has_effect(effect):
+	if effect is StatusEffect:
+		return status_effects.filter(func(e): return e.effect_name == effect.effect_name).size() > 0
+	else:
+		return status_effects.filter(func(e): return e.effect_name == effect).size() > 0
 
 func apply_effect(effect: StatusEffect):
 	if status_effects.has(effect):
 		var se_idx = status_effects.find(effect)
 		status_effects[se_idx].apply(time)
 	else:
-		status_effects.append(effect)
-		effect.apply(time)
+		var new_effect = effect.duplicate()
+		status_effects.append(new_effect)
+		new_effect.apply(time)
