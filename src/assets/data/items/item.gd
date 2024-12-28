42167fd41674951703d8e15722a66ac0dcf72959
Quinten Marsala <quinten.marsala@gmail.com>
2024-12-28 13:18:36 -0800

    expand item to include weapon, don't love this approach, but it works for now

--- src/assets/data/items/item.gd
+++ src/assets/data/items/item.gd
@@ -2,6 +2,9 @@ class_name Item
 extends Resource
 
 @export var item_id: Enums.Items
+@export var scene: PackedScene
 @export var name: String
 @export var description: String
-@export var scene: PackedScene
+#bleh - but trying to avoid circular ref
+@export var is_weapon: bool
+@export var weapon_data: WeaponData
