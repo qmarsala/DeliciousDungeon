e887bfc45e6bed55fe223fe4d130379980efe9ef
Quinten Marsala <quinten.marsala@gmail.com>
2024-12-28 13:18:15 -0800

    create 'weaponabilities' and ability data

--- src/assets/data/abilities/ability_data.gd
+++ src/assets/data/abilities/ability_data.gd
@@ -8,6 +8,8 @@ extends Resource
 
 @export var cast_time = .25
 @export var cooldown = 1.0
-@export var input_event = "ability_one"
 @export var damage: Array[float] = [1]
 @export var status_effects: Array[StatusEffect]
+
+# this feels like its in the wrong spot
+@export var input_event = "ability_one"
---
+++ src/assets/data/abilities/weapon_abilities/weapon_ability_data.gd
@@ -0,0 +1,5 @@
+class_name WeaponAbilityData
+extends Resource
+
+@export var weapon_ability_component: PackedScene
+@export var ability_data: AbilityData
