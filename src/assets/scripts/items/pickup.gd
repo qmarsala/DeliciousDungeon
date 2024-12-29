a1c6d84b346a202c0d6683177715e60e0c045125
Quinten Marsala <quinten.marsala@gmail.com>
2024-12-28 21:31:38 -0800

    use item method

--- src/assets/scripts/items/pickup.gd
+++ src/assets/scripts/items/pickup.gd
@@ -3,13 +3,8 @@ class_name Pickup
 
 @export var item: Item
 
-# todo: this node really should just encompas the area and the call to pickup
-# and not be to concerned about the item.  but if we ever wanted to have 
-# mouse over to see detail of a weapon pickup, that data would not yet be there
-# currently...
 func _ready():
-	var scene = item.scene
-	var instance = scene.instantiate()
+	var instance = item.create_item_scene()
 	add_child(instance)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
