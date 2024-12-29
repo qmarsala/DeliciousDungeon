28d8a08f576d9817be8e097fbccf20cca419b2fc
Quinten Marsala <quinten.marsala@gmail.com>
2024-12-28 20:50:22 -0800

    improve itemdata structure

--- src/assets/data/items/item.gd
+++ src/assets/data/items/item.gd
@@ -1,10 +1,11 @@
 class_name Item
 extends Resource
 
-@export var item_id: Enums.Items
+@export var data: ItemData
 @export var scene: PackedScene
-@export var name: String
-@export var description: String
-#bleh - but trying to avoid circular ref
-@export var is_weapon: bool
-@export var weapon_data: WeaponData
+
+func create_item_scene() -> Node:
+	var instance = scene.instantiate()
+	if instance.has_method("use_data"):
+		instance.use_data(data)
+	return instance
---
+++ src/assets/data/items/item_data.gd
@@ -0,0 +1,6 @@
+class_name ItemData
+extends Resource
+
+@export var item_id: Enums.Items
+@export var name: String
+@export var description: String
