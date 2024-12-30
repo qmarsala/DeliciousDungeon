9d4ebb1fbd2895cf83d75e49d514a73491b95005
Quinten Marsala <quinten.marsala@gmail.com>
2024-12-29 18:26:33 -0800

    add short delay to prevent pickup a drop

--- src/assets/scripts/items/pickup.gd
+++ src/assets/scripts/items/pickup.gd
@@ -3,11 +3,18 @@ class_name Pickup
 
 @export var item: Item
 
+var delay: float = .5
+var time: float = 0 #should we have a master time service?
+
 func _ready():
 	var instance = item.create_item_scene()
 	add_child(instance)
 
+func _process(delta: float) -> void:
+	time += delta
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	if time < delay: return
 	if body.has_method("pickup"):
 		body.pickup(item)
 		queue_free()
