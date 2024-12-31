154b160f651f23969c0bc0f2512cee5815e4676c
Quinten Marsala <quinten.marsala@gmail.com>
2024-12-30 19:04:41 -0800

    remove uneeded scrt

--- src/assets/scripts/attack_dummy.gd
+++
@@ -1,6 +0,0 @@
-extends Sprite2D
-
-@onready var status_effects_component: StatusEffectComponent = $StatusEffectsComponent
-	
-func receive_damage(damage):
-	SignalBusService.DamageReceived.emit(damage, global_position, false)
