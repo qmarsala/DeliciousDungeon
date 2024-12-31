a42eb2d16810042fe32c835ffbeb6960f2fc346e
Quinten Marsala <quinten.marsala@gmail.com>
2024-12-30 16:07:04 -0800

    use new attack in hitbox to apply data from the attack

--- src/assets/scripts/components/hitbox.gd
+++ src/assets/scripts/components/hitbox.gd
@@ -1,12 +1,15 @@
 class_name Hitbox
 extends Area2D
 
-@export var node: Node
+@export var health: HealthComponent
+@export var status_effects: StatusEffectComponent
+
+func init(health_component: HealthComponent, status_effects_component: StatusEffectComponent):
+	health = health_component
+	status_effects = status_effects_component
 
 func _ready() -> void:
-	add_to_group(Interfaces.Damageable)
+	add_to_group(Interfaces.Attackable)
 
-# should we just signal here?
-func receive_damage(damage: float) -> void:
-	if node.is_in_group(Interfaces.Damageable):
-		node.receive_damage(damage)
+func apply_attack(attack: Attack) -> void:
+	attack.apply(health, status_effects)
