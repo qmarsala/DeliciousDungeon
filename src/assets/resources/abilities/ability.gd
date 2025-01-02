6ea1028c9a100556fd16b40505d84ca769d41024
Quinten Marsala <quinten.marsala@gmail.com>
2025-01-01 22:52:11 -0800

    resolve potential circular ref in ability data

---
+++ src/assets/resources/abilities/ability.gd
@@ -0,0 +1,8 @@
+class_name Ability
+extends Resource
+
+@export var scene: PackedScene
+@export var data: AbilityData
+
+# this feels like its in the wrong spot
+@export var input_event = "ability_one"
--- src/assets/resources/abilities/ability_data.gd
+++ src/assets/resources/abilities/ability_data.gd
@@ -1,17 +1,11 @@
 class_name AbilityData
 extends Resource
 
-@export var has_scene: bool
-@export var scene: PackedScene
-@export var animation_name: String = "attack"
-
 @export var target_self: bool
+@export var animation_name: String = "attack"
 @export var ability_sound: AudioStream
 @export var cast_time = .3
 @export var cooldown = 1.3
 @export var damage: float = 1
 @export var status_effects: Array[StatusEffect] = []
 @export var status_effect_synergy: StatusEffectSynergy
-
-# this feels like its in the wrong spot
-@export var input_event = "ability_one"
