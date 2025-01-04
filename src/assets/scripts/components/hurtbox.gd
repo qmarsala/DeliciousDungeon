f71a3adab6f487805061c5c39e9523648a3fb472
Quinten Marsala <quinten.marsala@gmail.com>
2025-01-03 16:00:13 -0800

    free on collision

--- src/assets/scripts/components/hurtbox.gd
+++ src/assets/scripts/components/hurtbox.gd
@@ -2,8 +2,9 @@ class_name Hurtbox
 extends Area2D
 
 var attack: Attack
+var free_on_collision: bool
 
-func init(attack_data: Attack, apply_on_entered: bool):
+func init(attack_data: Attack, apply_on_entered: bool, free_on_collision: bool = false):
 	attack = attack_data
 	if apply_on_entered:
 		area_entered.connect(on_area_entererd)
@@ -16,3 +17,5 @@ func apply_attack():
 func on_area_entererd(area: Area2D) -> void:
 	if area is Hitbox:
 		area.apply_attack(attack)
+		if free_on_collision:
+			queue_free()
