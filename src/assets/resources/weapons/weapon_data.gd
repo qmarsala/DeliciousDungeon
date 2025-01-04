0bdb694ea9d99a79544b248508a3c919d600af04
Quinten Marsala <quinten.marsala@gmail.com>
2025-01-03 16:27:05 -0800

    clean up more weapon/attack stuff

--- src/assets/resources/weapons/weapon_data.gd
+++ src/assets/resources/weapons/weapon_data.gd
@@ -4,5 +4,6 @@ extends ItemData
 @export var max_range: float
 @export var cooldown_reduction: float = 0 # represents a perecentage subracted from cooldown
 #what other ways can we have weapon stats. 
+# if this is an array of ability data, where would the scene be found?
 @export var weapon_abilities: Array[Ability]
 @export var weapon_level: int = 1
