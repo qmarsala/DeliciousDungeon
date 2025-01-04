615fde37aa1d566df1730cf444fede2406cf03d3
Quinten Marsala <quinten.marsala@gmail.com>
2025-01-04 14:37:20 -0800

    idea for ability id?

--- src/assets/resources/abilities/ability.gd
+++ src/assets/resources/abilities/ability.gd
@@ -1,8 +1,10 @@
 class_name Ability
 extends Resource
 
+# idea: could we use an id and use that for ability binding and look up the scene and data
+# from an ability registry? 
+# 1XX melee, 2XX, magic, 3XX range?
+@export var ability_id: int
+
 @export var scene: PackedScene
 @export var data: AbilityData
-
-# this feels like its in the wrong spot
-@export var input_event = "ability_one"
