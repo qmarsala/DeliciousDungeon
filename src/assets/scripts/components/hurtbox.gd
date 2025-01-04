74c1eb1501ba11c52e61e2f9d920dcbfa235d83d
Quinten Marsala <quinten.marsala@gmail.com>
2025-01-03 16:16:52 -0800

    more hurtbox config

--- src/assets/scripts/components/hurtbox.gd
+++ src/assets/scripts/components/hurtbox.gd
@@ -4,10 +4,16 @@ extends Area2D
 var attack: Attack
 var free_on_collision: bool
 
-func init(attack_data: Attack, apply_on_entered: bool, free_on_collision: bool = false):
+func init(attack_data: Attack, 
+	attacks_enemy: bool = true,
+	attacks_player: bool = false,
+	apply_on_entered: bool = false, 
+	free_on_collision: bool = false):
 	attack = attack_data
 	if apply_on_entered:
 		area_entered.connect(on_area_entererd)
+	set_collision_mask_value(6, attacks_player)
+	set_collision_mask_value(7, attacks_enemy)
 
 func apply_attack():
 	var targets = get_overlapping_areas()
