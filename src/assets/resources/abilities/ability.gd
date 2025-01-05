c4f67444294bb659315230bc50869e9485a4820c
Quinten Marsala <quinten.marsala@gmail.com>
2025-01-04 17:34:22 -0800

    remove input event from ability resource, add projectile data to ability data

--- src/assets/resources/abilities/ability.gd
+++ src/assets/resources/abilities/ability.gd
@@ -3,6 +3,3 @@ extends Resource
 
 @export var scene: PackedScene
 @export var data: AbilityData
-
-# this feels like its in the wrong spot
-@export var input_event = "ability_one"
--- src/assets/resources/abilities/ability_data.gd
+++ src/assets/resources/abilities/ability_data.gd
@@ -6,6 +6,19 @@ extends Resource
 @export var ability_sound: AudioStream
 @export var cast_time = .3
 @export var cooldown = 1.3
-@export var damage: float = 1
+@export var show_castbar: bool = true
+@export var damage: float = 0
+@export var heal: float = 0
 @export var status_effects: Array[StatusEffect] = []
 @export var status_effect_synergy: StatusEffectSynergy
+
+# projectile data: do they make sense here?
+@export var speed = 300.0
+@export var can_pierce = false
+@export var max_pierce_count = 1
+@export var max_range = 0 # maybe weapons can have bonus range as they level? # how could this fit with melee abilities?
+
+func apply_weapon_stats(weapon: WeaponData) -> AbilityData:
+	var ability_data: AbilityData = self.duplicate()
+	ability_data.cooldown -= cooldown * weapon.cooldown_reduction
+	return ability_data
