70a540349c9be306e29f955bf8c86d217c130367
Quinten Marsala <quinten.marsala@gmail.com>
2025-01-07 17:04:10 -0800

    move evasion and armour check to apply attack

--- src/assets/scripts/objects/attack.gd
+++ src/assets/scripts/objects/attack.gd
@@ -4,11 +4,18 @@ var damage: float
 var status_effects: Array[StatusEffect] = []
 var effect_synergy: StatusEffectSynergy
 
-func apply(health_component: HealthComponent, status_effects_component: StatusEffectComponent) -> void:
+func apply(health_component: HealthComponent, 
+	status_effects_component: StatusEffectComponent,
+	armour_value: float = 0,
+	evasion_value: float = 0) -> void:
 	if status_effects_component:
 		if effect_synergy and status_effects_component.has_effect(effect_synergy.status_effect):
 			effect_synergy.apply(self)
 		for effect in status_effects:
 			status_effects_component.apply_effect(effect)
 	if health_component and damage > 0:
-		health_component.receive_damage(damage)
+		#moved armour poc: hit box is where we apply an attack, does it make sense for it to happen here then?
+		if randf() <= evasion_value:
+			health_component.receive_damage(-1)
+		else:
+			health_component.receive_damage(damage - armour_value)
