0d6babfb433715d5f2a433b1e8ef80e37187fb8b
Quinten Marsala <quinten.marsala@gmail.com>
2025-01-07 17:04:25 -0800

    let hand manage weapon based on equip events

--- src/assets/scripts/player/hand.gd
+++ src/assets/scripts/player/hand.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
 var character: Player
+var weapon: Weapon
 @export var reach: float = 8
 
 func _ready() -> void:
 	# I feel this is safe to assume hands.
 	character = get_parent() as Player
+	character.equipped_weapon.connect(on_weapon_equip)
+	character.unequipped_weapon.connect(on_weapon_unequip)
 	z_as_relative = false
 
 func _process(delta: float) -> void:
@@ -15,7 +18,15 @@ func _process(delta: float) -> void:
 		position = (direction.normalized() * reach) + Vector2(0,3)
 	else:
 		position = direction + Vector2(0, 3)
-	if position.y < 0:
+	if position.y < 1:
 		z_index = character.z_index - 1
 	else:
 		z_index = character.z_index
+
+func on_weapon_equip(weapon: Weapon):
+	self.weapon = weapon
+	add_child.call_deferred(weapon)
+
+func on_weapon_unequip():
+	if weapon:
+		weapon.queue_free.call_deferred()
