b60998d9f93dbc1a66909f149c07b9b2ac4db05f
Quinten Marsala <quinten.marsala@gmail.com>
2025-01-11 16:45:48 -0800

    start making more interesting dungeon layouts

--- src/assets/scripts/dungeon_manager.gd
+++ src/assets/scripts/dungeon_manager.gd
@@ -1,11 +1,6 @@
 class_name DungeonManager
 extends Node2D
 
-#todo: track what floor you are on, and go back to previous floor/next floor
-# idea: node for each floor, to make going back and forth easier?
-# problem: do we put the floors in different areas far enough apart we know
-# it wont be a problem? or do we 'disable and hide' the previous and next floors?
-
 var player: Player
 var current_floor: int = 0
 
@@ -14,8 +9,4 @@ func _ready() -> void:
 	for child in children:
 		if child is Player:
 			player = child
-			return
-
-
-# todo: signal on global bus to change scene should happen here
-# use internal signals to go between floors
+			break
