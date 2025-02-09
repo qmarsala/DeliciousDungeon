b9b4e370ecb4cb05369b99d225936c4b0401af88
Quinten Marsala <quinten.marsala@gmail.com>
2025-02-09 14:20:53 -0800

    first few scripts for the final build

---
+++ src/final/scripts/item_stack.gd
@@ -0,0 +1,6 @@
+class_name ItemStack
+extends RefCounted
+
+
+var count: int = 0
+var item: RefCounted # todo: Item model
---
+++ src/final/scripts/player_inventory.gd
@@ -0,0 +1,7 @@
+class_name PlayerInventory
+extends Resource
+
+@export var items: Dictionary = {}
+
+# func add_item
+# func remove_item
