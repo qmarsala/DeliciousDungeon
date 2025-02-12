65526cbc2aedfcac35b9d06700b48b40617a4580
Quinten Marsala <quinten.marsala@gmail.com>
2025-02-11 16:04:50 -0800

    player controller for gamepad movement

---
+++ src/final/scripts/player_movement_controller.gd
@@ -0,0 +1,29 @@
+class_name PlayerMovementController
+extends RefCounted
+
+#todo: player data speed
+var speed = 45.0
+
+func get_velocity(current_velocity: Vector2) -> Vector2:
+	var xDirection = get_x_input(current_velocity)
+	var yDirection = get_y_input(current_velocity)
+	var velocity = calc_player_velocity(xDirection, yDirection)
+	return velocity
+
+# is Input the right thing to use? or _unhandled_input with InputEvent?
+func get_x_input(current_velocity: Vector2):
+	var xDirection := Input.get_axis("move_west", "move_east")
+	if xDirection:
+		return xDirection
+	else:
+		return move_toward(current_velocity.x, 0, speed)
+
+func get_y_input(current_velocity: Vector2):
+	var yDirection = Input.get_axis("move_north", "move_south")
+	if yDirection:
+		return yDirection
+	else:
+		return move_toward(current_velocity.y, 0, speed)
+
+func calc_player_velocity(xDirection, yDirection):
+	return Vector2(xDirection, yDirection).normalized() * speed
