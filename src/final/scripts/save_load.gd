a0effb64205e9059f68d438632d2e6204991cffc
Quinten Marsala <quinten.marsala@gmail.com>
2025-02-15 15:06:12 -0800

    add func to reset game data

--- src/final/scripts/save_load.gd
+++ src/final/scripts/save_load.gd
@@ -5,12 +5,15 @@ var save_game_path = "user://game_data.res"
 
 func load_game_data() -> GameData2:
 	if not ResourceLoader.exists(save_game_path):
-		save_game_data(GameData2.new())
+		reset_game_data()
 	var saved_game_data = ResourceLoader.load(save_game_path)
 	if saved_game_data:
 		return saved_game_data
 	else:
 		return GameData2.new()
+
+func reset_game_data() -> void:
+	save_game_data(GameData2.new())
 
 func save_game_data(game_data: GameData2) -> void:
 	var result = ResourceSaver.save(game_data, save_game_path)
