cad8bbcee5bc9cee5fc136a485862dc9870fa7dc
Quinten Marsala <quinten.marsala@gmail.com>
2025-02-16 10:35:47 -0800

    start establishing final player interface

--- src/final/scripts/player/player.gd
+++ src/final/scripts/player/player.gd
@@ -1,2 +1,26 @@
 class_name Player2
 extends CharacterBody2D
+
+func attack() -> void:
+	pass
+	
+func equip() -> void:
+	pass
+	
+func unequip() -> void:
+	pass
+
+func switch_weapons() -> void:
+	pass
+
+func pickup() -> void:
+	pass
+
+func drop() -> void:
+	pass
+
+func cook() -> void:
+	pass
+
+func consume() -> void:
+	pass
