925b5606cad855c7859bf221e478ac54af72ae7f
Quinten Marsala <quinten.marsala@gmail.com>
2025-02-16 15:53:11 -0800

    fix bad type name

--- src/final/scripts/save_load.gd
+++ src/final/scripts/save_load.gd
@@ -3,19 +3,19 @@ extends RefCounted
 
 var save_game_path = "user://game_data.res"
 
-func load_game_data() -> GameData2:
+func load_game_data() -> GameData:
 	if not ResourceLoader.exists(save_game_path):
 		reset_game_data()
 	var saved_game_data = ResourceLoader.load(save_game_path)
 	if saved_game_data:
 		return saved_game_data
 	else:
-		return GameData2.new()
+		return GameData.new()
 
 func reset_game_data() -> void:
-	save_game_data(GameData2.new())
+	save_game_data(GameData.new())
 
-func save_game_data(game_data: GameData2) -> void:
+func save_game_data(game_data: GameData) -> void:
 	var result = ResourceSaver.save(game_data, save_game_path)
 	if result != OK:
 		printerr("Unable to save game!")
