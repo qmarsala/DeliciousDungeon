0cd219a1e4887c49e5f666d13901ae5c76e6fd1d
Quinten Marsala <quinten.marsala@gmail.com>
2025-02-17 14:21:45 -0800

    re org files

--- src/final/scripts/scene_management/scene_manager.gd
+++ src/final/scripts/scene_management/scene_manager.gd
@@ -11,30 +11,29 @@ extends Node
 @onready var animation_player: AnimationPlayer = $TransitionLayer/AnimationPlayer
 
 var world: Node
+var player_data: PlayerData2
 
 var next_scene: PackedScene
-var next_scene_level: int
-var current_scene_instance: Node
+var next_scene_level: int = 0
+var current_scene_instance: GameScene
 
-var changing_scene: bool = false
-
-func _ready() -> void:
-	world = get_tree().get_first_node_in_group("World")
-	if not world: 
+func init(world: Node, player_data: PlayerData2) -> void:
+	self.world = world
+	if not self.world: 
 		printerr("Could not find world node, scene manager not initialized!")
 		return
-		
-	if SignalBus.SceneChange.is_connected(queue_scene_change):
-		SignalBus.SceneChange.disconnect(queue_scene_change)
-	SignalBus.SceneChange.connect(queue_scene_change)
-
-func queue_scene_change(event: SceneChangeEvent) -> void:
-	print("queue_scene_change")
 	
-	if changing_scene:
-		return 
-	changing_scene = true
-	world.process_mode = Node.PROCESS_MODE_DISABLED
+	self.player_data = player_data
+	if not self.player_data: 
+		printerr("Could not find player_data, scene manager not initialized!")
+		return
+	
+	current_scene_instance = main_menu_scene.instantiate()
+	current_scene_instance.init(self.player_data)
+	world.add_child(current_scene_instance)
+
+func queue_scene_change(event: SceneChangeRequestedEvent) -> void:
+	print("queue_scene_change")
 	
 	next_scene_level = event.level
 	if event.scene_type == Enums.Scenes.Main:
@@ -42,30 +41,23 @@ func queue_scene_change(event: SceneChangeEvent) -> void:
 	elif event.scene_type == Enums.Scenes.Outdoors:
 		next_scene = outdoor_scenes.pick_random()
 	else:
-		if event.level < 4:
+		if next_scene_level < 4:
 			next_scene = canyon_dungeon_scene
-		elif event.level < 9:
+		elif next_scene_level < 9:
 			next_scene = cave_dungeon_scene
 		else:
 			next_scene = fort_dungeon_scene
 	
+	world.process_mode = Node.PROCESS_MODE_DISABLED
 	animation_player.play("scene_change")
-	
 
 func perform_scene_change() -> void:
 	print("preform_scene_change")
-	
-	if current_scene_instance:
-		current_scene_instance.queue_free()
 	for c in world.get_children():
-		if c.is_in_group("Player"):
-			c.global_position = Vector2.ZERO
-		else:
-			c.queue_free()
+		c.queue_free()
 	
-	current_scene_instance = next_scene.instantiate()
-	current_scene_instance.init(next_scene_level)
+	current_scene_instance = next_scene.instantiate() as GameScene
+	current_scene_instance.init(player_data, next_scene_level)
 	
-	world.add_child.call_deferred(current_scene_instance)
+	world.add_child(current_scene_instance)
 	world.process_mode = Node.PROCESS_MODE_INHERIT
-	changing_scene = false
