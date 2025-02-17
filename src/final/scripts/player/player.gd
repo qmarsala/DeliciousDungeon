d75419534e47a1f2e1d687fad21d6264ccbffabf
Quinten Marsala <quinten.marsala@gmail.com>
2025-02-16 16:30:19 -0800

    inject player data

--- src/final/scripts/player/player.gd
+++ src/final/scripts/player/player.gd
@@ -1,13 +1,16 @@
 class_name Player2
 extends CharacterBody2D
 
+var data: PlayerData2
 
 var player_move_controller = PlayerMovementController.new()
 
-#todo: movment component - call get direction
 func _physics_process(delta: float) -> void:
 	velocity = player_move_controller.get_velocity(velocity)
 	move_and_slide()
+
+func init(player_data: PlayerData2) -> void:
+	data = player_data
 
 func attack() -> void:
 	pass
@@ -31,4 +34,7 @@ func cook() -> void:
 	pass
 
 func consume() -> void:
+	pass
+
+func save() -> void:
 	pass
