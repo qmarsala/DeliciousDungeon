02f2e83ecebaf88ef3ba6b7cfff78f2c8bd75bea
Quinten Marsala <quinten.marsala@gmail.com>
2025-02-17 16:29:16 -0800

    just pass game data to scene manager

--- src/final/scripts/scene_management/scene_manager.gd
+++ src/final/scripts/scene_management/scene_manager.gd
@@ -11,39 +11,38 @@ extends Node
 @onready var animation_player: AnimationPlayer = $TransitionLayer/AnimationPlayer
 
 var world: Node
-var player_data: PlayerData2
+var game_data: GameData
 
 var next_scene: PackedScene
 var next_scene_level: int = 0
 var current_scene_instance: GameScene
 
-func init(world: Node, player_data: PlayerData2) -> void:
+func init(world: Node, game_data: GameData) -> void:
 	self.world = world
 	if not self.world: 
 		printerr("Could not find world node, scene manager not initialized!")
 		return
 	
-	self.player_data = player_data
-	if not self.player_data: 
-		printerr("Could not find player_data, scene manager not initialized!")
+	self.game_data = game_data
+	if not self.game_data: 
+		printerr("Could not find game_data, scene manager not initialized!")
 		return
 	
 	current_scene_instance = main_menu_scene.instantiate()
-	current_scene_instance.init(self.player_data)
+	current_scene_instance.init(game_data.player_data, game_data.current_level)
 	world.add_child(current_scene_instance)
 
-func queue_scene_change(scene_type: Enums.Scenes, level: int = 0) -> void:
+func queue_scene_change(scene_type: Enums.Scenes) -> void:
 	print("queue_scene_change")
 	
-	next_scene_level = level
 	if scene_type == Enums.Scenes.Main:
 		next_scene = main_menu_scene
 	elif scene_type == Enums.Scenes.Outdoors:
 		next_scene = outdoor_scenes.pick_random()
 	else:
-		if level < 4:
+		if game_data.current_level < 4:
 			next_scene = canyon_dungeon_scene
-		elif level < 9:
+		elif game_data.current_level < 9:
 			next_scene = cave_dungeon_scene
 		else:
 			next_scene = fort_dungeon_scene
@@ -57,7 +56,7 @@ func perform_scene_change() -> void:
 		c.queue_free()
 	
 	current_scene_instance = next_scene.instantiate() as GameScene
-	current_scene_instance.init(player_data, next_scene_level)
+	current_scene_instance.init(game_data.player_data, game_data.current_level)
 	
 	world.add_child(current_scene_instance)
 	world.process_mode = Node.PROCESS_MODE_INHERIT
