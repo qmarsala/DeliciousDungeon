8ecece2dcde76f64af0175ce69c07c817486959e
Quinten Marsala <quinten.marsala@gmail.com>
2025-02-19 18:06:59 -0800

    use item types in inv

--- src/final/scripts/resources/player_inventory.gd
+++ src/final/scripts/resources/player_inventory.gd
@@ -4,48 +4,48 @@ extends Resource
 @export var weapons: Dictionary = {}
 @export var items: Dictionary = {}
 
-func has_weapon(weapon_data: WeaponData2) -> bool:
-	var key = _get_weapon_key(weapon_data)
+func has_weapon(weapon: Weapon2) -> bool:
+	var key = _get_weapon_key(weapon)
 	if key.is_empty():
 		return false
 	return weapons.has(key)
 
-func add_weapon(weapon_data: WeaponData2) -> void:
-	var key = _get_weapon_key(weapon_data)
+func add_weapon(weapon: Weapon2) -> void:
+	var key = _get_weapon_key(weapon)
 	if key.is_empty():
 		return
-	weapons[key] = weapon_data
+	weapons[key] = weapon
 
-func remove_weapon(weapon_data: WeaponData2) -> void:
-	var key = _get_weapon_key(weapon_data)
-	if has_weapon(weapon_data):
+func remove_weapon(weapon: Weapon2) -> void:
+	var key = _get_weapon_key(weapon)
+	if has_weapon(weapon):
 		weapons.erase(key)
 
-func has_item(item_data: ItemData) -> bool:
-	var key = _get_item_key(item_data)
+func has_item(item: Item) -> bool:
+	var key = _get_item_key(item)
 	if key.is_empty():
 		return false
 	return items.has(key)
 
-func add_item(item_data: ItemData, count: int = 1) -> void:
-	var key = _get_item_key(item_data)
+func add_item(item: Item, count: int = 1) -> void:
+	var key = _get_item_key(item)
 	if key.is_empty() or count < 1:
 		return
 	
-	if has_item(item_data):
+	if has_item(item):
 		items[key].count += count
 	else:
 		items[key] = ItemStack.new()
-		items[key].data = item_data
+		items[key].item = item
 		items[key].count = count
 
-func remove_item(item_data: ItemData, count: int) -> ItemStack:
-	var key = _get_item_key(item_data)
+func remove_item(item: Item, count: int) -> ItemStack:
+	var key = _get_item_key(item)
 	if key.is_empty() or count < 1:
 		return
 
-	var stack = _default_stack(item_data.name)
-	if has_item(item_data):
+	var stack = _default_stack(item.data.name)
+	if has_item(item):
 		stack.data = items[key].data
 		var actual_taken = min(count, items[key].count)
 		stack.count = actual_taken
@@ -54,19 +54,19 @@ func remove_item(item_data: ItemData, count: int) -> ItemStack:
 			items.erase(key)
 	return stack
 
-func _get_weapon_key(weapon_data: WeaponData2) -> String:
-	if weapon_data == null:
+func _get_weapon_key(weapon: Weapon2) -> String:
+	if weapon == null:
 		return ""
-	elif weapon_data.weapon_type == Enums.WeaponTypes.Melee:
+	elif weapon.weapon_type == Enums.WeaponTypes.Melee:
 		return "melee"
 	else:
 		return "ranged"
 
-func _get_item_key(item_data: ItemData) -> String:
-	if item_data == null:
+func _get_item_key(item: Item) -> String:
+	if item == null:
 		return ""
 	else:
-		return item_data.name.to_lower()
+		return str(item.data.item_id)
 
 func _default_stack(name: String) -> ItemStack:
 	var stack = ItemStack.new()
