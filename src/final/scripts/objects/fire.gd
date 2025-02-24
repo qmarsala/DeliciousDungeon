fe168b7ec21bdfc25656aa6e86624524bbbaa6ca
Quinten Marsala <quinten.marsala@gmail.com>
2025-02-23 18:56:12 -0800

    fires die out after some time

--- src/final/scripts/objects/fire.gd
+++ src/final/scripts/objects/fire.gd
@@ -12,18 +12,31 @@ extends Node2D
 @export var animation_name: String = "campfire_light_flicker"
 @export var lit: bool = true
 @export var lit_chance: float = .25
+@export var lit_lifetime_seconds: float = 30
+
+var lit_at: float = 0
 
 func _ready() -> void:
 	interactbox.interacted.connect(interact)
 	lit = lit || randf() > 1 - lit_chance
 	if lit:
+		lit_at = GameTime.time
 		animation_player.play(animation_name)
+
+func _process(delta: float) -> void:
+	if not lit: 
+		return
+	
+	if GameTime.time - lit_at >= lit_lifetime_seconds:
+		lit = false
+		animation_player.stop(false)
+		animation_player.play("RESET")
 
 func interact(player: Player2) -> void:
 	if lit:
 		print("open cooking menu")
 	else:
-		light()
+		player.light_fire(self)
 
 func light() -> void:
 	if lit: return
